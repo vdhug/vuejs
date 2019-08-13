0ed0c0188eb9a5b31ab6fc8e7d93ab0ceaccdc2a
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-12 22:27:06 -0300

    Fetching produtos info da fake api, e adicionando estilo a lista de produtos

--- techno/app.js
+++ techno/app.js
@@ -1,6 +1,18 @@
 const app = new Vue({
   el: "#app",
   data: {
-    mensagem: "Ola mundo",
+    produtos: [],
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        });
+    }
+  },
+  created() {
+    this.fetchProdutos();
   }
 });
