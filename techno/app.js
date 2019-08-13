99909b19d6770301c52fa5baf0a320d01164e71c
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-13 13:50:27 -0300

    Criando router para personalização de url

--- techno/app.js
+++ techno/app.js
@@ -85,15 +85,27 @@ const app = new Vue({
       setTimeout(() => {
         this.alertaAtivo = false;
       }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash) {
+        this.fetchProduto(hash.replace("#", ""));
+      }
     }
   },
   watch: {
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
     }
   },
   created() {
     this.fetchProdutos();
     this.checarLocalStorage();
+    this.router();
   }
 });
