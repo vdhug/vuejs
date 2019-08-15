e677f63a2a39ece8db2a3c26c882d67668d9cb4b
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-14 22:13:20 -0300

    Utilizando slots dentro dos componentes

--- vue-cli/app-vue/src/App.vue
+++ vue-cli/app-vue/src/App.vue
@@ -2,15 +2,30 @@
   <div id="app">
     <h1>{{mensagem}}</h1>
     <header-principal></header-principal>
+    <modal-principal>
+      <template v-slot:header>
+        <p>Header do componente modal</p>
+      </template>
+      <template v-slot:footer>
+        <p>Footer do componente modal</p>
+      </template>
+
+      <template v-slot:default="{user}">
+        <h3>{{user}}</h3>
+        <p>Esse é um teste</p>
+      </template>
+    </modal-principal>
   </div>
 </template>
 
 <script>
 import HeaderPrincipal from "./components/HeaderPrincipal.vue";
+import ModalPrincipal from "./components/ModalPrincipal.vue";
 
 export default {
   components: {
-    HeaderPrincipal
+    HeaderPrincipal,
+    ModalPrincipal
   },
   data() {
     return {
---
+++ vue-cli/app-vue/src/components/ModalPrincipal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal">
+    <slot name="header"></slot>
+    <h2>{{user.nome}}</h2>
+    <h1>Modal Principal</h1>
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalPrincipal",
+  data() {
+    return {
+      user: {
+        nome: "Vitor Hugo"
+      }
+    };
+  }
+};
+</script>
