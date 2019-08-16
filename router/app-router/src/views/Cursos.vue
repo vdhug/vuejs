8c7c08851281459b27b1e815cccee3cdb47efe33
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-15 23:36:30 -0300

    Introducao aos conceitos do vue router

---
+++ router/app-router/src/views/Cursos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Cursos</h1>
+    <ul>
+      <li>Javascript</li>
+      <li>CSS com SASS</li>
+      <li>Vue JS</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
---
+++ router/app-router/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Essa é a home</h1>
+</template>
+
+
+<script>
+export default {};
+</script>
