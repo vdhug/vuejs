cedf6790835167ce7f3e619c0a07f03f3a95f2b4
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-19 22:21:24 -0300

    Finalizando estilo do componente de formulario com dados do usuario que sera reaproveitado nos momentos de registrar usuario, editar e finalizar compra

--- ranek/src/components/LoginCriar.vue
+++ ranek/src/components/LoginCriar.vue
@@ -1,10 +1,42 @@
 <template>
-  <p>LoginCrar</p>
+  <section>
+    <h2>Crie sua conta</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" @click="criar = true" class="btn">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
-export default {};
+import UsuarioForm from "@/components/UsuarioForm.vue";
+export default {
+  data() {
+    return {
+      criar: false
+    };
+  },
+  components: {
+    UsuarioForm
+  }
+};
 </script>
 
 <style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.btn-form {
+  max-width: 100%;
+}
 </style>
